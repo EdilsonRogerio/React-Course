dddb8160e28df44c11193b401e5697c07a53df1f
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-22 00:05:48 -0300

    props

--- components/src/index.js
+++ components/src/index.js
@@ -2,40 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Edilson"
-        timeAdo="Hoje as 4:45 AM"
-        comment="Hello"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Shelton"
-        timeAdo="Hoje as 14:45 PM"
-        comment="HI"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Yuran"
-        timeAdo="14 de setembro de 2022"
-        comment="How you doing"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Charlen"
-        timeAdo="Há dois dias"
-        comment="Nice to meet ya!!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Shirley"
-        timeAdo="Há 4 semanas"
-        comment="Olá!!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Edilson"
+          timeAgo="Hoje as 4:45 AM"
+          comment="Hello"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Shelton"
+          timeAgo="Hoje as 14:45 PM"
+          comment="HI"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Yuran"
+          timeAgo="14 de setembro de 2022"
+          comment="How you doing"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Charlen"
+          timeAgo="Há dois dias"
+          comment="Nice to meet ya!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Shirley"
+          timeAgo="Há 4 semanas"
+          comment="Olá!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
