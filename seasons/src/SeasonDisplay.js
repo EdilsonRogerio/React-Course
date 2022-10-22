aaf8fe92f66814dfefbc8864f10d462fecd4369e
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-22 02:37:30 -0300

    Class components

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+    return (
+        <h1>Verão</h1>
+    );
+}
+
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+class App extends React.Component {
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <h1>Latitude: </h1>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
