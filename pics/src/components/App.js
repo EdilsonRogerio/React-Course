19603371f3d45f21a68f1716b4be4c3308dd4bcf
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-24 20:02:20 -0300

    Request results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     onSearchSubimt(term) {
-        console.log(term);
+        axios.get("https://api.unsplash.com/search/photos", {
+            params: {
+                query: term,
+            },
+            headers: {
+                Authorization: "Client-ID HcdUQD6OeV7sJZBaUdby59OllC4XneLwsxOUkgof2Dc",
+            }
+        });
     }
     render () {
         return (
