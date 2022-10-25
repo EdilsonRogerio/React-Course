773f56da136709e5dd5ed7418c14309800ec9487
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-24 23:15:03 -0300

    Image card component

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,15 +1,17 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-    const images = props.images.map(({ description, id, urls }) => {
+    const images = props.images.map((image) => {
         return (
-            <div>
-                <img alt={description} key={id} src={urls.regular} />
-            </div>
+            <ImageCard
+                key={image.id} image={image}
+            />
         );
     })
     return (
-        <div>
+        <div className="image-list">
             {images}
         </div>
     );
