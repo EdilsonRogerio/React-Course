c9497be7bdb40ac8e704b6f93581fafa6b535062
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-10-28 13:38:55 -0300

    Terminando o app videos

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,8 +1,20 @@
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     return (
-        <div>{video.snipet.title}</div>
+        <div className="video-item item" onClick={() => onVideoSelect(video)}>
+            <img 
+                alt={video.snippet.title} 
+                className="ui image" 
+                src={video.snippet.thumbnails.medium.url} 
+            />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </div>
     );
 }
 
