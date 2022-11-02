696c9bbbbf3c58c3bcb2abd24392dbc52d92abe9
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-11-02 16:52:54 -0300

    Refactoring Search and App

---
+++ youtube/src/components/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import Youtube from "../apis/Youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    useEffect(() => {
+        onInputSubmit("Ricky and morty");
+    }, []);
+
+    const onInputSubmit = async input => {
+        const response = await Youtube.get("/search", {
+            params: {
+                q: input,
+            }
+        });
+
+        setVideos(response.data.items);
+        setSelectedVideo(response.data.items[0]);
+    };
+
+    const onVideoSelect = (video) => {
+        setSelectedVideo(video);
+    };
+
+    return (
+        <div className="ui container">
+            <SearchBar
+                onFormSubmit={onInputSubmit}
+            />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail
+                            video={selectedVideo}
+                        />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                            videos={videos}
+                            onVideoSelect={onVideoSelect}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+
+};
+
+export default App;
