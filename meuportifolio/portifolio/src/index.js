cc21324fe76abe3c08a84f894a5189dda0293000
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-11-04 00:34:22 -0300

    portifolio

---
+++ meuportifolio/portifolio/src/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <div>
+        <h1>React 18</h1>
+        <p>React 18 is here!</p>
+    </div>
+);
